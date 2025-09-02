9cce37f6c376c35cbab7318c936ed4c4006c341e
Oliver <olee7149@gmail.com>
2025-09-02 10:45:58 +0800

    add get_source_metadata_fred func

--- R/fred_data.R
+++ R/fred_data.R
@@ -44,9 +44,58 @@ get_source_utime_fred <- function(series_id, config) {
   mode <- config$mode
   
   url <- sprintf("%s?series_id=%s&api_key=%s&file_type=%s", url, series_id, api_key, mode)
-  data <- jsonlite::fromJSON(url)
+  # data <- jsonlite::fromJSON(url)
+  res <- curl::curl_fetch_memory(url)
+  data <- jsonlite::fromJSON(rawToChar(res$content), simplifyVector = TRUE)
   
   update_time_str <- data$seriess$last_updated # UTC+5 by defult
   as.POSIXct(update_time_str, format = "%Y-%m-%d %H:%M:%S", tz = "UTC")
+}
+
+#' Retrieve metadata for a FRED series
+#'
+#' Queries the FRED API to obtain basic metadata of a given series, such as
+#' title, observation range, frequency, units, and seasonal adjustment.
+#'
+#' @param series_id A character string. The FRED series ID (e.g., "AMERIBOR").
+#' @param config A list containing API configuration with elements:
+#'   \describe{
+#'     \item{api_key}{Your FRED API key as a character string.}
+#'     \item{url}{Base URL of the FRED API endpoint for series (e.g., "https://api.stlouisfed.org/fred/series").}
+#'     \item{mode}{File type to request, usually "json".}
+#'   }
+#'
+#' @return A list with elements:
+#' \itemize{
+#'   \item \code{title} — Series title
+#'   \item \code{start} — Observation start date
+#'   \item \code{end} — Observation end date
+#'   \item \code{freq} — Data frequency
+#'   \item \code{units} — Measurement units
+#'   \item \code{season} — Seasonal adjustment type
+#' }
+#'
+#' @examples
+#' \dontrun{
+#' config <- list(
+#'   api_key = "your_api_key",
+#'   url = "https://api.stlouisfed.org/fred/series",
+#'   mode = "json"
+#' )
+#' get_source_metadata_fred("AMERIBOR", config)
+#' }
+#'
+#' @export
+get_source_metadata_fred <- function(series_id, config) {
+  api_key <- config$api_key
+  url <- config$url
+  mode <- config$mode
+  
+  url <- sprintf("%s?series_id=%s&api_key=%s&file_type=%s", url, series_id, api_key, mode)
+  res <- curl::curl_fetch_memory(url)
+  data <- jsonlite::fromJSON(rawToChar(res$content), simplifyVector = TRUE)
+  
+  res <- data$seriess
+  list(title = res$title, start = res$observation_start, end = res$observation_end, freq = res$frequency, units = res$units, season = res$seasonal_adjustment)
 }
 
